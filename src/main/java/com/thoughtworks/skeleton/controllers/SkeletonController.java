b52d20f2277d8b71fd623d5a459cdac87d321237
Max Anderson <manderso@thoughtworks.com>
2016-11-17 14:38:36 -0800

    initial commit

---
+++ src/main/java/com/thoughtworks/skeleton/controllers/SkeletonController.java
@@ -0,0 +1,29 @@
+package com.thoughtworks.skeleton.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+import java.security.Principal;
+import java.util.Optional;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@RestController
+public class SkeletonController {
+    @RequestMapping(method = GET)
+    public ResponseEntity<String> hi() {
+        return new ResponseEntity<>("HELLO", OK);
+    }
+}
---
+++ src/test/java/com/thoughtworks/skeleton/controllers/SkeletonControllerTest.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.skeleton.controllers;
+
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SkeletonControllerTest {
+
+    private final SkeletonController skeletonController = new SkeletonController();
+
+    @Test
+    public void shouldReturnHello() {
+        ResponseEntity<String> hi = skeletonController.hi();
+
+        assertThat(hi.getStatusCode(), is(HttpStatus.OK));
+        assertThat(hi.getBody(), is("HELLO"));
+    }
+
+}
