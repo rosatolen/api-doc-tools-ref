9e8c0f5a4b78c31061fc1532abd98f4b102751bc
Max Anderson <manderso@thoughtworks.com>
2016-12-20 16:06:41 -0800

    adding test profile to other integration tests

--- src/integration/java/com/thoughtworks/apidoc/controllers/UserControllerIntegrationTest.java
+++ src/integration/java/com/thoughtworks/apidoc/controllers/UserControllerIntegrationTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static io.restassured.RestAssured.given;
@@ -17,6 +18,7 @@ import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.http.HttpStatus.CREATED;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles({"local", "test"})
 @SpringBootTest(classes = APIReferenceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerIntegrationTest {
 
